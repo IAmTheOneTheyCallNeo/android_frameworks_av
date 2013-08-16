03ed9864e637f4060acbe1d27289d2f1affa03dc
David Eddlemon <DavidEddlemon@Hotmail.com>
2013-08-17 01:22:43 +0400

    Strict Aliasing Fix

--- media/libmediaplayerservice/Android.mk
+++ media/libmediaplayerservice/Android.mk
@@ -71,7 +71,7 @@ endif
 LOCAL_MODULE:= libmediaplayerservice
 
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
-    LOCAL_CFLAGS += -DENABLE_QC_AV_ENHANCEMENTS
+    LOCAL_CFLAGS += -DENABLE_QC_AV_ENHANCEMENTS -fno-strict-aliasing
     LOCAL_C_INCLUDES += $(TOP)/frameworks/av/include/media
     ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
         LOCAL_C_INCLUDES += \
